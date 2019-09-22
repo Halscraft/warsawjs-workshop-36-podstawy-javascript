b457fddd576beae590ca7075733f0e6fd6500580
Matełko <mateusz.hals@halscraft.pl>
2019-09-22 16:20:07 +0200

    Commit po omówieniu krótkim funkcji.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// function min(a,b) {
+//     if (a<b) {
+//          result = "A jest mniejsze."
+//     } else
+//     {
+//          result = "B jest mniejsze."
+//     }
+// }
+// var result
+// min(4,5);
+// console.log(result);
+
+
+// function potega(x,n) {
+//     wynik = Math.pow(x,n)
+// }
+
+// var wynik;
+// potega(2,3)
+// console.log(wynik);
+
+const currentDate = new Date();
+console.log(currentDate.getFullYear());
+
+//lub krótszy zapis
+
+// console.log((new Date()).getHours());
